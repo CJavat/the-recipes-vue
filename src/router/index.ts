b024cc5344bd5026df0b98675a3bee937e92e8c5
CJavatX <cdpm98@hotmail.com>
2024-10-11 12:11:01 -0600

    Modificaciones en las rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { dashboardRoutes } from '@/modules/dashboard/routes'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     authRoutes,
     dashboardRoutes,
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [vue(), mode !== 'production' && vueDevTools()].filter(Boolean), // Esto elimina valores `false` de la lista
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-}))
+})
